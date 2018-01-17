ebcace9fa02c8432b94890e2fed26fee3df70606
kbsmith01 <kbsmith01@TS15864.ysu.local>
2018-01-17 12:10:36 -0500

    Initial program framework written.

---
+++ src/java_class_prg_00_02_object_intro/Demo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_class_prg_00_02_object_intro;
+
+import java.util.Stack;
+/**
+ *
+ * @author kbsmith01
+ */
+public class Demo {
+    public static void mani(String[] args) {
+        Stack s1 = new Stack();
+        Stack s2 = new Stack();
+        s1.push("Larry");
+    }
+}
